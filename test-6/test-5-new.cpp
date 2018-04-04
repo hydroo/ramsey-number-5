d1a71f37bab5fcdb280cfc7f9ed997dfc59770c2
Ronny Brendel <ronnybrendel@gmail.com>
2018-04-04 17:11:33 -0400

    pull r >= n, s >= n check out of the inner loop

--- test-6/test-5-new.cpp
+++ test-6/test-5-new.cpp
@@ -134,41 +134,41 @@ bool allColoringsHaveCompleteOrEmptySubgraph(const std::array<std::vector<Adjace
 
         if (coloring.edge(edge) == true) {
 
-            // Compare this graph against all appropriate complete subgraphs.
-            // Appropriate means every graph whos complete subgraph's laste edge is exactly the lastly enumerated edge
-            bool foundCompleteSubgraph = false;
-            const auto& currentEdgeMasksComplete = edgeMasksCompleteByLastOne[edge + 1];
-            R5_BENCH(*edgeMaskChecks += currentEdgeMasksComplete.size());
-            for (std::size_t i = 0; i < currentEdgeMasksComplete.size(); i += 1) {
-                if ((coloring & currentEdgeMasksComplete[i]) == currentEdgeMasksComplete[i]) {
-                    if (config::n >= config::r) {  // avoids matching subgraphs larger than the to-be-checked graph
+            if (config::n >= config::r) { // avoids matching subgraphs larger than the to-be-checked graph
+                // Compare this graph against all appropriate complete subgraphs.
+                // Appropriate means every graph whos complete subgraph's laste edge is exactly the lastly enumerated edge
+                bool foundCompleteSubgraph = false;
+                const auto& currentEdgeMasksComplete = edgeMasksCompleteByLastOne[edge + 1];
+                R5_BENCH(*edgeMaskChecks += currentEdgeMasksComplete.size());
+                for (std::size_t i = 0; i < currentEdgeMasksComplete.size(); i += 1) {
+                    if ((coloring & currentEdgeMasksComplete[i]) == currentEdgeMasksComplete[i]) {
                         // cerr << "    " << currentEdgeMasksComplete[i] << " is complete subgraph" << endl;
                         R5_BENCH(*coloringsChecked += 1);
                         foundCompleteSubgraph = true;
                         break;
                     }
                 }
+                if (foundCompleteSubgraph) { continue; }
             }
-            if (foundCompleteSubgraph) { continue; }
 
         } else {
 
-            // Do the same for empty subgraphs
-            bool foundEmptySubgraph = false;
-            const auto& currentEdgeMasksEmpty = edgeMasksEmptyByLastZero[edge + 1];
-            R5_BENCH(*edgeMaskChecks += currentEdgeMasksEmpty.size());
-            for (std::size_t i = 0; i < currentEdgeMasksEmpty.size(); i += 1) {
-                if ((coloring | currentEdgeMasksEmpty[i]) == currentEdgeMasksEmpty[i]) {
-                    if (config::n >= config::s) {  // avoids matching subgraphs larger than the to-be-checked graph
+            if (config::n >= config::s) { // avoids matching subgraphs larger than the to-be-checked graph
+                // Do the same for empty subgraphs
+                bool foundEmptySubgraph = false;
+                const auto& currentEdgeMasksEmpty = edgeMasksEmptyByLastZero[edge + 1];
+                R5_BENCH(*edgeMaskChecks += currentEdgeMasksEmpty.size());
+                for (std::size_t i = 0; i < currentEdgeMasksEmpty.size(); i += 1) {
+                    if ((coloring | currentEdgeMasksEmpty[i]) == currentEdgeMasksEmpty[i]) {
                         // cerr << "    " << currentEdgeMasksEmpty << " is empty subgraph" << endl;
                         R5_BENCH(*coloringsChecked += 1);
                         foundEmptySubgraph = true;
                         break;
                     }
                 }
+                if (foundEmptySubgraph) { continue; }
             }
 
-            if (foundEmptySubgraph) { continue; }
         }
 
         if (edge < config::e - 1) {
