4abd85a3454ddafaf806c403e24c2ae352a5f341
Ronny Brendel <ronnybrendel@gmail.com>
2018-10-03 12:21:44 -0400

    micro fix: doesn't need to be default

--- test-7/adjacencymatrix.hpp
+++ test-7/adjacencymatrix.hpp
@@ -722,7 +722,7 @@ public:
 
 } // namespace r5
 
-template<s64 Nodes, bool Triangular = true>
+template<s64 Nodes, bool Triangular>
 std::ostream& operator<< (std::ostream& o, const r5::AdjacencyMatrix<Nodes, Triangular>& m) {
     o << m.print();
     return o;
